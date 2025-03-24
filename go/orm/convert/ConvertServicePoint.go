7348f6c96c236bc2deebf265db0fc0d6aefb6eb3
Sharon Aicler <saichler@cisco.com>
2025-03-24 07:02:12 -0700

    Stash changes

---
+++ go/orm/convert/ConvertServicePoint.go
@@ -0,0 +1,64 @@
+package convert
+
+import (
+	"errors"
+	"github.com/saichler/l8orm/go/types"
+	"github.com/saichler/types/go/common"
+	types2 "github.com/saichler/types/go/types"
+	"google.golang.org/protobuf/proto"
+)
+
+const (
+	TOPIC    = "Convert"
+	ENDPOINT = "convert"
+)
+
+type ConvertServicePoint struct {
+}
+
+func RegisterConvertCenter(area int32, resources common.IResources) {
+	this := &ConvertServicePoint{}
+	err := resources.ServicePoints().RegisterServicePoint(area, nil, this)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (this *ConvertServicePoint) Post(pb proto.Message, resourcs common.IResources) (proto.Message, error) {
+	return ConvertTo(pb, resourcs)
+}
+func (this *ConvertServicePoint) Put(pb proto.Message, resourcs common.IResources) (proto.Message, error) {
+	return nil, nil
+}
+func (this *ConvertServicePoint) Patch(pb proto.Message, resourcs common.IResources) (proto.Message, error) {
+	return nil, nil
+}
+func (this *ConvertServicePoint) Delete(pb proto.Message, resourcs common.IResources) (proto.Message, error) {
+	return nil, nil
+}
+func (this *ConvertServicePoint) Get(pb proto.Message, resourcs common.IResources) (proto.Message, error) {
+	data, ok := pb.(*types.RelationalData)
+	if !ok {
+		return nil, errors.New("Data is not of type *types.RelationalData")
+	}
+	pbs, err := ConvertFrom(data, resourcs)
+	if err != nil {
+		return nil, err
+	}
+	return pbs.(proto.Message), nil
+}
+func (this *ConvertServicePoint) GetCopy(pb proto.Message, resourcs common.IResources) (proto.Message, error) {
+	return nil, nil
+}
+func (this *ConvertServicePoint) Failed(pb proto.Message, resourcs common.IResources, msg *types2.Message) (proto.Message, error) {
+	return nil, nil
+}
+func (this *ConvertServicePoint) EndPoint() string {
+	return ENDPOINT
+}
+func (this *ConvertServicePoint) Topic() string {
+	return TOPIC
+}
+func (this *ConvertServicePoint) Transactional() bool   { return false }
+func (this *ConvertServicePoint) ReplicationCount() int { return 0 }
+func (this *ConvertServicePoint) ReplicationScore() int { return 0 }
